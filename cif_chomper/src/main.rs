df20a03b2c8782a5773dc6bafdba8f3ec137fc8b
perl_d <david.perl@psi.ch>
2026-03-27 21:02:18 +0100

    fix: paths for publish build

--- cif_chomper/src/main.rs
+++ cif_chomper/src/main.rs
@@ -1,7 +1,7 @@
 use cif_chomper_core::parser::cif2_file;
 
 fn main() {
-    let raw_content = include_str!("../../cif_chomper/templ_enum.cif");
+    let raw_content = include_str!("../templ_enum.cif");
     let model = cif2_file(raw_content).expect("parse fail");
     dbg!(model);
 }
