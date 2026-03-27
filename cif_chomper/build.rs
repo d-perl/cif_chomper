f914006db4ab0febd403162dbd8645b2b8a9dfd5
perl_d <david.perl@psi.ch>
2026-03-27 21:20:10 +0100

    fix: codegen to OUT_DIR

--- cif_chomper/build.rs
+++ cif_chomper/build.rs
@@ -3,7 +3,8 @@ use std::{fs::File, io::BufWriter, path::Path};
 use cif_chomper_core::codegen::cif_vessel_codegen;
 
 fn main() {
-    let dst_path = Path::new("./src/__cif_vessel.rs");
+    let path = std::env::var("OUT_DIR").unwrap() + "/cif_vessel.rs";
+    let dst_path = Path::new(&path);
     let mut file = BufWriter::new(File::create(dst_path).unwrap());
 
     let templ_enum = include_str!("./templ_enum.cif");
